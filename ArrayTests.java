2da99eb90f7da8e564fe29b4c3726455635a6ff5
DiegoIno1 <dinostroza@ucsd.edu>
2024-02-01 09:16:50 -0800

    Checkpoint thing

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,23 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = { 3, 5, 0 };
+    int[] expectedOutput = {0, 5, 3};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(expectedOutput, input2);
 	}
 
+ 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = { 3, 5, 0 };
+    int[] expectedOutput = {0, 5, 3};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(expectedOutput, ArrayExamples.reversed(input2));
   }
+
 }
